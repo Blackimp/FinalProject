b482d01f47febf1bf21559f27a190d63ea9a3343
Blackimp <j.neuberth@gmail.com>
2013-05-21 21:13:32 +0200

    Movie Top 20 implementiert, Links zu Testzwecken durch Buttons ersetzt

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,24 @@ var image = "";
 var pic_index = 0;
 var myMovieTable_string = '';
 
+$(document).ready(function() {
+
+	$('#myMovies').click(function() {
+		$("#myMoviesTable tr").empty();
+		getMyMovies();
+	});
+
+	$('#friendsMovies').click(function() {
+		$("#myMoviesTable tr").empty();
+		getFriendMovies();
+	});
+
+	$('#recommended').click(function() {
+		// to implement
+	});
+
+});
+
 // setup the facebook application and load the facebook SDK
 function facebookLogin() {
 	// initialize
@@ -16,9 +34,7 @@ function facebookLogin() {
 
 		FB.Event.subscribe('auth.authResponseChange', function(response) {
 			if (response.status === 'connected') {
-			      $('#facebookButton').hide();
-				getMyMovies();
-				getFriendMovies();
+				$('#facebookButton').hide();
 			} else if (response.status === 'not_authorized') {
 				FB.login({
 					scope : 'user_likes,friends_likes,read_friendlists'
@@ -57,11 +73,11 @@ function getMyMovies() {
 		var movies = movies_string.split(', ');
 
 		for (var i = 0; i < movies.length; i++) {
-		      if(i == movies.length-1) {
-				getMovieCover(movies[i],1);
-		      } else {
-		      	getMovieCover(movies[i],0);
-		      }
+			if (i == movies.length - 1) {
+				getMovieCover(movies[i], 1);
+			} else {
+				getMovieCover(movies[i], 0);
+			}
 		};
 	});
 };
@@ -73,49 +89,83 @@ function getFriendMovies() {
 	}, function(data) {
 		var obj = data.data;
 		var movies_string = "";
-		var movies = "";
+		var movies_friend = "";
+		var movies_all = new Array();
+		var movie_counts = {};
+		var movie_counts_sorted = [];
 
 		Object.keys(obj).forEach(function(key) {
-			// check if the user has set no hometown in his profile
+			// check if the user likes no movies
 			if (obj[key].movies == "") {
 				return;
 			} else {
 				movies_string = obj[key].movies;
-				movies = movies_string.split(', ');
 
-				var text = "";
-				for (var i = 0; i < movies.length; i++) {
-					text += movies[i] + "\n";
-				}
+				// splits the returned string into single movies
+				movies_friend = movies_string.split(', ');
+
+				// append the friends movies to the array that should contain all movies
+				movies_all.push.apply(movies_all, movies_friend);
 			}
 		});
+
+		// add a counter to the movies
+		for ( i = 0; i < movies_all.length; i++) {
+			var count = movie_counts[movies_all[i]];
+			if (count != null) {
+				count++;
+			} else {
+				count = 1;
+			}
+			movie_counts[movies_all[i]] = count;
+		};
+
+		// sorts the movies after the count they have been liked
+		for (var movie in movie_counts) {
+			movie_counts_sorted.push([movie, movie_counts[movie]]);
+		};
+
+		movie_counts_sorted.sort(function(a, b) {
+			return b[1] - a[1];
+		});
+
+		// take only the 20 most liked movies
+		var movies_top20 = movie_counts_sorted.slice(0, 20);
+
+		for ( i = 0; i < movies_top20.length; i++) {
+			if (i == movies_top20.length - 1) {
+				getMovieCover(movies_top20[i,i][0], 1);
+			} else {
+				getMovieCover(movies_top20[i,i][0], 0);
+			}
+		};
 	});
 };
 
-
 function output(movie, image, last_item) {
-      if(pic_index == 0) {
-            myMovieTable_string += "<tr>";
-      } else if(pic_index % 5 == 0) {
-            myMovieTable_string += "</tr><tr>";
-      }
+	if (pic_index == 0) {
+		myMovieTable_string += "<tr>";
+	} else if (pic_index % 5 == 0) {
+		myMovieTable_string += "</tr><tr>";
+	}
 	myMovieTable_string += "<td><img src='" + image + "' alt='" + movie + "'></td>";
-	if(last_item == 1) {
-	      myMovieTable_string += "</tr>";
-	      $('#myMoviesTable').append(myMovieTable_string);
+	if (last_item == 1) {
+		myMovieTable_string += "</tr>";
+		$('#myMoviesTable').append(myMovieTable_string);
 	}
 	pic_index++;
-	
+
 };
 
 function getMovieCover(movie, last_item) {
+	console.log(movie);
 	var api_key = "bcc2dc80864852143b71c43ccdc9df30";
 	var url = "http://api.themoviedb.org/3/search/movie?query=" + movie + "&api_key=" + api_key;
 	$.getJSON(url, function(data) {
 		var obj = data;
 		if (obj.results[0] == undefined) {
 			return;
-		} else if (obj.results[0].poster_path == null){
+		} else if (obj.results[0].poster_path == null) {
 			return;
 		} else {
 			image = "http://cf2.imgobject.com/t/p/w500" + obj.results[0].poster_path;
