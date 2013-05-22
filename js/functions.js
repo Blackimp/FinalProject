eeb6d9dd5973e8511e3e06d3a74f303c79c0f411
Kevin Ventzke <kevin.ventzke@gmail.com>
2013-05-22 18:27:17 +0200

    setting my branch on master level

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 var image = "";
 var pic_index = 0;
-var myMovieTable_string = '';
+var content_table = '<table>';
+
 
 // setup the facebook application and load the facebook SDK
 function facebookLogin() {
@@ -16,9 +17,7 @@ function facebookLogin() {
 
 		FB.Event.subscribe('auth.authResponseChange', function(response) {
 			if (response.status === 'connected') {
-			      $('#facebookButton').hide();
-				getMyMovies();
-				getFriendMovies();
+				$('#facebookButton').hide();
 			} else if (response.status === 'not_authorized') {
 				FB.login({
 					scope : 'user_likes,friends_likes,read_friendlists'
@@ -46,6 +45,8 @@ function facebookLogin() {
 };
 
 function getMyMovies() {
+      clearContentTable();
+
 	var query = 'SELECT movies FROM user WHERE uid = me()';
 	// call the Facebook API using the fql
 	FB.api('fql', {
@@ -57,55 +58,89 @@ function getMyMovies() {
 		var movies = movies_string.split(', ');
 
 		for (var i = 0; i < movies.length; i++) {
-		      if(i == movies.length-1) {
-				getMovieCover(movies[i],1);
-		      } else {
-		      	getMovieCover(movies[i],0);
-		      }
+			if (i == movies.length - 1) {
+				getMovieCover(movies[i], 1);
+			} else {
+				getMovieCover(movies[i], 0);
+			}
 		};
 	});
 };
 
 function getFriendMovies() {
+      clearContentTable();
+      
 	var query = 'SELECT movies, name FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1=me())';
 	FB.api('fql', {
 		q : query
 	}, function(data) {
 		var obj = data.data;
 		var movies_string = "";
-		var movies = "";
+		var movies_friend = "";
+		var movies_all = new Array();
+		var movie_counts = {};
+		var movie_counts_sorted = [];
 
 		Object.keys(obj).forEach(function(key) {
-			// check if the user has set no hometown in his profile
+			// check if the user likes no movies
 			if (obj[key].movies == "") {
 				return;
 			} else {
 				movies_string = obj[key].movies;
-				movies = movies_string.split(', ');
 
-				var text = "";
-				for (var i = 0; i < movies.length; i++) {
-					text += movies[i] + "\n";
-				}
+				// splits the returned string into single movies
+				movies_friend = movies_string.split(', ');
+
+				// append the friends movies to the array that should contain all movies
+				movies_all.push.apply(movies_all, movies_friend);
 			}
 		});
+
+		// add a counter to the movies
+		for ( i = 0; i < movies_all.length; i++) {
+			var count = movie_counts[movies_all[i]];
+			if (count != null) {
+				count++;
+			} else {
+				count = 1;
+			}
+			movie_counts[movies_all[i]] = count;
+		};
+
+		// sorts the movies after the count they have been liked
+		for (var movie in movie_counts) {
+			movie_counts_sorted.push([movie, movie_counts[movie]]);
+		};
+
+		movie_counts_sorted.sort(function(a, b) {
+			return b[1] - a[1];
+		});
+
+		// take only the 20 most liked movies
+		var movies_top20 = movie_counts_sorted.slice(0, 20);
+
+		for ( i = 0; i < movies_top20.length; i++) {
+			if (i == movies_top20.length - 1) {
+				getMovieCover(movies_top20[i,i][0], 1);
+			} else {
+				getMovieCover(movies_top20[i,i][0], 0);
+			}
+		};
 	});
 };
 
-
 function output(movie, image, last_item) {
-      if(pic_index == 0) {
-            myMovieTable_string += "<tr>";
-      } else if(pic_index % 5 == 0) {
-            myMovieTable_string += "</tr><tr>";
-      }
-	myMovieTable_string += "<td><img src='" + image + "' alt='" + movie + "'></td>";
-	if(last_item == 1) {
-	      myMovieTable_string += "</tr>";
-	      $('#myMoviesTable').append(myMovieTable_string);
+	if (pic_index == 0) {
+		content_table += "<tr>";
+	} else if (pic_index % 5 == 0) {
+		content_table += "</tr><tr>";
+	}
+	content_table += "<td><img src='" + image + "' alt='" + movie + "' onmouseover='showPic(\""+image+"\");'></td>";
+	if (last_item == 1) {
+		content_table += "</tr></table>";
+		document.getElementById('content').innerHTML = content_table;
 	}
 	pic_index++;
-	
 };
 
 function getMovieCover(movie, last_item) {
@@ -115,7 +150,7 @@ function getMovieCover(movie, last_item) {
 		var obj = data;
 		if (obj.results[0] == undefined) {
 			return;
-		} else if (obj.results[0].poster_path == null){
+		} else if (obj.results[0].poster_path == null) {
 			return;
 		} else {
 			image = "http://cf2.imgobject.com/t/p/w500" + obj.results[0].poster_path;
@@ -123,3 +158,13 @@ function getMovieCover(movie, last_item) {
 		}
 	});
 };
+
+function clearContentTable() {
+      pic_index = 0;
+      document.getElementById("content").innerHTML = '';
+      content_table = '<table>';
+}
+
+function showPic(image) {
+      document.getElementById('pic_screen').innerHTML = "<img src="+image+">";
+}
