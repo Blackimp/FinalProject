000c9c3dd46b4a4de0242a67a4ac7d25363a0dca
Kevin Ventzke <kevin.ventzke@gmail.com>
2013-05-23 15:31:58 +0200

    dumme kacke

---
+++ js/artistFunctions.js
@@ -0,0 +1,41 @@
+function getMyArtists() {
+	clearContentTable();
+
+	var query = 'SELECT music FROM user WHERE uid = me()';
+	// call the Facebook API using the fql
+	FB.api('fql', {
+		q : query
+	}, function(data) {
+		var obj = data.data;
+		var artists_string = obj[0].music;
+
+		var artists = artists_string.split(', ');
+
+		for (var i = 0; i < artists.length; i++) {
+			if (i == artists.length - 1) {
+				getArtist(artists[i], 1);
+			} else {
+				getArtist(artists[i], 0);
+			}
+		};
+	});
+};
+
+function getArtist(artist, last_item) {
+	var url = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key=b15fbe5f05f68e2b62bbbe3f4242c303&format=json";
+
+	$.getJSON(url, function(data) {
+		var obj = data.results;
+
+		if (obj.artistmatches.artist == undefined) {
+			return;
+		} else if (obj.artistmatches.artist[0] == undefined) {
+			return;
+		} else {
+			// here is a problem with the JSON response, because a # is in the element tag before "text". This is why we have to work with a temp variable.
+			var tmp = obj.artistmatches.artist[0].image[3];
+			var image = tmp['#text'];
+			output(artist, image, last_item);
+		}
+	});
+};
---
+++ js/bookFunctions.js
@@ -0,0 +1,34 @@
+function getMyBooks() {
+	clearContentTable();
+
+	var query = 'SELECT books FROM user WHERE uid = me()';
+	// call the Facebook API using the fql
+	FB.api('fql', {
+		q : query
+	}, function(data) {
+		var obj = data.data;
+		var books_string = obj[0].books;
+
+		var books = books_string.split(', ');
+		console.log(books);
+		for (var i = 0; i < books.length; i++) {
+			if (i == books.length - 1) {
+				getBookCover(books[i], 1);
+			} else {
+				getBookCover(books[i], 0);
+			}
+		};
+	});
+};
+
+function getBookCover(book, last_item) {
+	var url = "https://www.googleapis.com/books/v1/volumes?q=name:" + book;
+
+	$.getJSON(url, function(data) {
+		var obj = data;
+
+		var image = obj.items[0].volumeinfo.imageLinks.thumbnail;
+		console.log(image);
+		output(book, image, last_item);
+	});
+};
