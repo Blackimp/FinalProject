5ecc98e7778b42c50905108d06fb4d98d84f712a
Blackimp <j.neuberth@gmail.com>
2013-05-24 17:34:13 +0200

    get Data on pageload implemented

--- js/moviesFunctions.js
+++ js/moviesFunctions.js
@@ -1,90 +1,59 @@
 function getMyMovies() {
 	clearContentTable();
 
-	var query = 'SELECT movies FROM user WHERE uid = me()';
-	// call the Facebook API using the fql
-	FB.api('fql', {
-		q : query
-	}, function(data) {
-		var obj = data.data;
-		var movies_string = obj[0].movies;
-		if(movies_string == "") {
-                  document.getElementById('content').innerHTML = no_content_table;
-                  return;
-            }
+	if (myMovies == "") {
+		document.getElementById('content').innerHTML = no_content_table;
+		return;
+	};
 
-		var movies = movies_string.split(', ');
-
-		for (var i = 0; i < movies.length; i++) {
-			if (i == movies.length - 1) {
-				getMovieCover(movies[i],1, -1);
-			} else {
-				getMovieCover(movies[i], 0, -1);
-			}
-		};
-	});
+	for (var i = 0; i < myMovies.length; i++) {
+		if (i == myMovies.length - 1) {
+			getMovieCover(myMovies[i], 1, -1);
+		} else {
+			getMovieCover(myMovies[i], 0, -1);
+		}
+	};
 };
 
 function getFriendMovies() {
 	clearContentTable();
 
-	var query = 'SELECT movies FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1=me())';
-	FB.api('fql', {
-		q : query
-	}, function(data) {
-		var obj = data.data;
-		var movies_string = "";
-		var movies_friend = "";
-		var movies_all = new Array();
-		var movie_counts = {};
-		var movie_counts_sorted = [];
+	var movies_all = new Array();
+	var movie_counts = {};
+	var movie_counts_sorted = [];
 
-		Object.keys(obj).forEach(function(key) {
-			// check if the user likes no movies
-			if (obj[key].movies == "") {
-				return;
-			} else {
-				movies_string = obj[key].movies;
+	movies_all = friendsMovies;
 
-				// splits the returned string into single movies
-				movies_friend = movies_string.split(', ');
+	// add a counter to the movies
+	for ( i = 0; i < movies_all.length; i++) {
+		var count = movie_counts[movies_all[i]];
+		if (count != null) {
+			count++;
+		} else {
+			count = 1;
+		}
+		movie_counts[movies_all[i]] = count;
+	};
 
-				// append the friends movies to the array that should contain all movies
-				movies_all.push.apply(movies_all, movies_friend);
-			}
-		});
+	// sorts the movies after the count they have been liked
+	for (var movie in movie_counts) {
+		movie_counts_sorted.push([movie, movie_counts[movie]]);
+	};
 
-		// add a counter to the movies
-		for ( i = 0; i < movies_all.length; i++) {
-			var count = movie_counts[movies_all[i]];
-			if (count != null) {
-				count++;
-			} else {
-				count = 1;
-			}
-			movie_counts[movies_all[i]] = count;
-		};
-
-		// sorts the movies after the count they have been liked
-		for (var movie in movie_counts) {
-			movie_counts_sorted.push([movie, movie_counts[movie]]);
-		};
-
-		movie_counts_sorted.sort(function(a, b) {
-			return b[1] - a[1];
-		});
-
-		// take only the 20 most liked movies
-		var movies_top20 = movie_counts_sorted.slice(0, 20);
-		
-		for ( i = 0; i < movies_top20.length; i++) {
-			if (i == movies_top20.length - 1) {
-				getMovieCover(movies_top20[i,i][0], 1, movies_top20[i,i][1]);
-			} else {
-				getMovieCover(movies_top20[i,i][0], 0, movies_top20[i,i][1]);
-			}
-		};
+	movie_counts_sorted.sort(function(a, b) {
+		return b[1] - a[1];
 	});
+
+	// take only the 20 most liked movies
+	var movies_top20 = movie_counts_sorted.slice(0, 20);
+
+	for ( i = 0; i < movies_top20.length; i++) {
+		if (i == movies_top20.length - 1) {
+			getMovieCover(movies_top20[i,i][0], 1, movies_top20[i,i][1]);
+		} else {
+			getMovieCover(movies_top20[i,i][0], 0, movies_top20[i,i][1]);
+		}
+	};
 };
 
 function getMovieCover(movie, last_item, like_count) {
--- js/uiFunctions.js
+++ js/uiFunctions.js
@@ -3,19 +3,29 @@ var pic_index = 0;
 var init_content_table = '<table><colgroup><col width="100"><col width="100"><col width="100"><col width="100"><col width="100"></colgroup>';
 var no_content_table = '<div>No data available</div>';
 var content_table = init_content_table;
+var myMovies = new Array();
+var myMusic = new Array();
+var myBooks = new Array();
+var friendsMovies = new Array();
+var friendsMusic = new Array();
+var friendsBooks = new Array();
 
-function output(title, image,  last_item, like_count) {
+$(document).ready(function() {
+	facebookLogin();
+});
+
+function output(title, image, last_item, like_count) {
 	if (pic_index == 0) {
 		content_table += "<tr>";
 	} else if (pic_index % 5 == 0) {
 		content_table += "</tr><tr>";
 	}
 	content_table += "<td>";
-	
-	if(like_count != -1) {
-	      content_table += "<div class='like_count'>"+like_count + " likes</div>";
+
+	if (like_count != -1) {
+		content_table += "<div class='like_count'>" + like_count + " likes</div>";
 	}
-	
+
 	content_table += "<img src='" + image + "' alt='" + title + "' onmouseover='showPic(\"" + title + "\",\"" + image + "\");'></td>";
 	document.getElementById('content').innerHTML = content_table + "</tr></table>";
 
@@ -29,8 +39,8 @@ function clearContentTable() {
 	document.getElementById("pic_screen").innerHTML = '';
 }
 
-function showPic(title,image) {
-      var pic_string = '<div class="pic_title">'+title+'</div><img src='+image+'>';
+function showPic(title, image) {
+	var pic_string = '<div class="pic_title">' + title + '</div><img src=' + image + '>';
 	document.getElementById('pic_screen').innerHTML = pic_string;
 }
 
@@ -49,6 +59,8 @@ function facebookLogin() {
 		FB.Event.subscribe('auth.authResponseChange', function(response) {
 			if (response.status === 'connected') {
 				$('#facebookButton').hide();
+				getMyData();
+				getFriendsData();
 			} else if (response.status === 'not_authorized') {
 				FB.login({
 					scope : 'user_likes,friends_likes,read_friendlists'
@@ -73,4 +85,69 @@ function facebookLogin() {
 			js.src = "//connect.facebook.net/en_US/all.js";
 			ref.parentNode.insertBefore(js, ref);
 		}(document));
+};
+
+function getMyData() {
+	var query = 'SELECT movies, music, books FROM user WHERE uid = me()';
+
+	// call the Facebook API using the fql
+	FB.api('fql', {
+		q : query
+	}, function(data) {
+		var obj = data.data;
+		var movies_string = obj[0].movies;
+		var music_string = obj[0].music;
+		var books_string = obj[0].books;
+
+		myMovies = movies_string.split(', ');
+		myMusic = music_string.split(', ');
+		myBooks = books_string.split(', ');
+	});
+};
+
+function getFriendsData() {
+	var query = 'SELECT movies, music, books FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1=me())';
+	FB.api('fql', {
+		q : query
+	}, function(data) {
+		var obj = data.data;
+		var movies_friend = "";
+		var music_friend = "";
+		var books_friend = "";
+
+		Object.keys(obj).forEach(function(key) {
+			// check if the user likes no movies
+			if (obj[key].movies == "") {
+				return;
+			} else {
+				// splits the returned string into single movies
+				movies_friend = obj[key].movies.split(', ');
+
+				// append the friends movies to the array that should contain all movies
+				friendsMovies.push.apply(friendsMovies, movies_friend);
+			};
+			
+			// check if the user likes no music
+			if (obj[key].music == "") {
+				return;
+			} else {
+				// splits the returned string into single artists
+				music_friend = obj[key].music.split(', ');
+
+				// append the friends movies to the array that should contain all artists
+				friendsMusic.push.apply(friendsMusic, music_friend);
+			};
+			
+			// check if the user likes no books
+			if (obj[key].books == "") {
+				return;
+			} else {
+				// splits the returned string into single books
+				books_friend = obj[key].books.split(', ');
+
+				// append the friends movies to the array that should contain all books
+				friendsBooks.push.apply(friendsBooks, books_friend);
+			}
+		});
+	});
 };
